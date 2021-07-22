13a31aaf48ba5f7b7548046dcd4a4c120ea0f9de
gohdg <gohdg@naver.com>
2021-07-22 17:14:14 +0900

    app bar icon, AppBar( leading, actions) and IconButton widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,139 +1,53 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main()=> runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
-  @override
+@override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'BBANTO',
-      home: Grade(),
+      title: 'Appbar',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: MyPage(),
     );
   }
 }
 
-class Grade extends StatelessWidget {
+class MyPage extends StatelessWidget {
+  get onPressed => null;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: Text('BBANTO'),
+        title: Text('Appbar icon menu'),
         centerTitle: true,
-        backgroundColor: Colors.amber[700],
         elevation: 0.0,
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/flying.gif'),
-                radius: 60.0,                
-              ),
-            ),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[850],
-              thickness: 0.5,
-              endIndent: 30.0,
-            ),
-            Text('NAME',
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-            ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text('BBANTO',
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text('BBANTO POWER LEVEL',
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-            ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text('14',
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-              fontSize: 28.0,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text('using lightsaber',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0,
-                ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text('face hero tattoo',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0,
-                ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text('fire frames',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  letterSpacing: 1.0,
-                ),
-                ),
-              ],
-            ),
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/bbanto.png'),
-                radius: 40.0,
-                backgroundColor: Colors.amber[800],
-              ),
-            ),
-          ],
+        leading: IconButton(
+          icon: Icon(Icons.menu), 
+          onPressed: () { 
+            print('menu button is clicked');
+           },
         ),
+        actions: [
+          IconButton(
+          icon: Icon(Icons.shopping_cart), 
+          onPressed: () { 
+            print('Shopping cart button is clicked');
+           },
+          ),
+          IconButton(
+          icon: Icon(Icons.search), 
+          onPressed: () { 
+            print('Search button is clicked');
+           },
+          ),
+        ],
       ),
+      
     );
   }
 }
