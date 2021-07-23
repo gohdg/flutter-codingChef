b20dbaaec2f04f11bc815e1179dd4ac2125b3f40
gohdg <gohdg@naver.com>
2021-07-23 13:06:40 +0900

    SnacBar using ScaffoldMessenger.of(context).showSnackBar

--- lib/main.dart
+++ lib/main.dart
@@ -20,84 +20,25 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Appbar icon menu'),
+        title: Text('Snack Bar'),
         centerTitle: true,
         elevation: 0.0,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              print('Shopping cart button is clicked');
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              print('Search button is clicked');
-            },
-          ),
-        ],
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/bbanto.png'),
-                backgroundColor: Colors.white,
-              ),
-              otherAccountsPictures: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/chef.png'),
-                ),
-                // CircleAvatar(
-                //   backgroundImage: AssetImage('assets/chef.png'),
-                // ),
-              ],
-              accountName: Text('BBANTO'),
-              accountEmail: Text('david@netsbridge.com'),
-              onDetailsPressed: () {
-                print('arrow is clicked');
-              },
-              decoration: BoxDecoration(
-                  color: Colors.red[200],
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(40.0),
-                      bottomRight: Radius.circular(40.0))),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text('Hello')));
+          },
+          child: Text(
+            'Show me',
+            style: TextStyle(
+              color: Colors.white,
             ),
-            ListTile(
-              leading: Icon(
-                Icons.home,
-                color: Colors.grey[850],
-              ),
-              title: Text('Home'),
-              onTap: () {
-                print('Home is tapped');
-              },
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.settings,
-                color: Colors.grey[850],
-              ),
-              title: Text('Setting'),
-              onTap: () {
-                print('Setting is tapped');
-              },
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.question_answer,
-                color: Colors.grey[850],
-              ),
-              title: Text('Q&A'),
-              onTap: () {
-                print('Q&A is tapped');
-              },
-              trailing: Icon(Icons.add),
-            ),
-          ],
+          ),
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.red,
+          ),
         ),
       ),
     );
