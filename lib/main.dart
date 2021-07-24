fcd9c40a0df5d4b5a2bfff3e8b79bacf9ec4c6db
gohdg <gohdg@naver.com>
2021-07-24 21:55:18 +0900

    import each seperate dart file, MaterialApp(initialRoute: '/', routes: {'/': (context) => ScreenA(), 'b' : (context) => ScreenB(),})

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_app/ScreenA.dart';
+import 'package:flutter_app/ScreenB.dart';
+import 'package:flutter_app/ScreenC.dart';
 void main() {
   runApp(MyApp());
 }
@@ -9,55 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Myapp',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: FirstPage(),
-    );
-  }
-}
-
-class FirstPage extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context2) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('First page')
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Go to the Second page'),
-          onPressed: (){
-            Navigator.push(context2, MaterialPageRoute(
-                builder: (context) => SecondPage()
-            )
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-
-
-  @override
-  Widget build(BuildContext ctx) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Text('Second page')
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Go to the First page'),
-          onPressed: (){
-            Navigator.pop(ctx);
-          },
-        ),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/' : (context) => ScreenA(),
+        '/b' : (context) => ScreenB(),
+        '/c' : (context) => ScreenC()
+      },
     );
   }
 }
