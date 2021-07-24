a274a42c124c3aebd471a1b1231c5d7e9b4b92d2
gohdg <gohdg@naver.com>
2021-07-24 21:05:18 +0900

    Navigator, Navigator.push(context, MaterialPageRoute(builder:(context) => SecondPage())); , Navigator.pop(context);

--- lib/main.dart
+++ lib/main.dart
@@ -13,44 +13,50 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyPage(),
+      home: FirstPage(),
     );
   }
 }
-class MyPage extends StatelessWidget {
+
+class FirstPage extends StatelessWidget {
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context2) {
     return Scaffold(
-      backgroundColor: Colors.teal,
-      body: SafeArea(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              height: 100,
-              color: Colors.white,
-              child: Text('Container 1'),
-            ),
-            SizedBox(
-              width: 30,
-            ),
-            Container(
-              height: 100,
-              color: Colors.blue,
-              child: Text('Container 2'),
-            ),
-            SizedBox(
-              width: 60,
-            ),
-            Container(
-              height: 100,
-              color: Colors.red,
-              child: Text('Container 3'),
-            ),
+      appBar: AppBar(
+        title: Text('First page')
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to the Second page'),
+          onPressed: (){
+            Navigator.push(context2, MaterialPageRoute(
+                builder: (context) => SecondPage()
+            )
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
 
-          ],
-        ) ,
+class SecondPage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Second page')
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to the First page'),
+          onPressed: (){
+            Navigator.pop(ctx);
+          },
+        ),
       ),
     );
   }
