799455b091f688580b88ab4d28a8121c36054f9b
gohdg <gohdg@naver.com>
2021-07-26 12:22:47 +0900

    1. declare StatefulWidget, 2. add createSate(){}, 3. create MyAppState extends State<MyApp>{ }

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/ScreenA.dart';
-import 'package:flutter_app/ScreenB.dart';
-import 'package:flutter_app/ScreenC.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState(){
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp>{
   // This widget is the root of your application.
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/' : (context) => ScreenA(),
-        '/b' : (context) => ScreenB(),
-        '/c' : (context) => ScreenC()
-      },
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('You have pushed the button this many times:'),
+              Text(
+                '$counter',
+                style: Theme.of(context).textTheme.headline3,
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FloatingActionButton(
+                      onPressed: (){
+                        setState(() {
+                          counter++;
+                          print('$counter');
+                        });
+                      },
+                      child: Icon(Icons.add),
+                  ),
+                  SizedBox(
+
+                    width: 20.0,
+                  ),
+                  FloatingActionButton(
+                    onPressed: (){
+                      setState(() {
+                        counter--;
+                        print('$counter');
+                      });
+                    },
+                    child: Icon(Icons.remove),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+
+      ),
     );
   }
 }
